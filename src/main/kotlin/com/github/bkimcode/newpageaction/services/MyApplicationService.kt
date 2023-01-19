b7d013dac00e06a9d9109473ebda9ee20e4b13e4
GitHub Action <action@github.com>
2023-01-19 22:20:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/bkimcode/newpageaction/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bkimcode/newpageaction/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bkimcode.newpageaction.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bkimcode.newpageaction.MyBundle
 
 class MyApplicationService {
 
