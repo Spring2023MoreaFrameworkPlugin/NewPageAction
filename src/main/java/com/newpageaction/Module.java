8ebfa6059f066df8a84be2c16ff0efc2a3967375
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2023-02-02 10:50:05 -1000

    x

--- src/main/java/com/newpageaction/Module.java
+++ src/main/java/com/newpageaction/Module.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.InputValidator;
 import com.intellij.openapi.ui.Messages;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Module extends AnAction {
 
     @Override
@@ -23,6 +25,17 @@ public class Module extends AnAction {
         if (input != null) {
             // Do something with the input, for example, create a new file with the specified name
             // in the current project directory
+          try {
+            FileWriter myWriter = new FileWriter(input + ".md");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+          } catch (IOException n) {
+            System.out.println("An error occurred.");
+            n.printStackTrace();
+          }
+        }
+        else{
         }
     }
 }
