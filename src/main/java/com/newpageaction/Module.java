b8939c22a49962605c7058178d9c2b9414325f94
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2023-02-16 10:50:24 -1000

    done

--- src/main/java/com/newpageaction/Module.java
+++ src/main/java/com/newpageaction/Module.java
@@ -52,7 +52,7 @@ public class Module extends AnAction {
                     VirtualFile newFile = newDir.createChildData(this, "module" + "-" + input + ".md");
                     // Create the file so it matches
                     try (OutputStream outputStream = newFile.getOutputStream(this)) {
-                        String content = "---\ntitle: \"" + input + "\"\npublished: true\nmorea_coming_soon: false\nmorea_id: "+ input+"-module \nmorea_outcomes:\nmorea_readings:\nmorea_experiences:\nmorea_assessments:\nmorea_type: module\nmorea_icon_url:\nmorea_start_date:\nmorea_end_date:\nmorea_labels:\nmorea_sort_order:\n---\n\n## " + input + "\n\nThis is a sample content for the newly created .md file.";
+                        String content = "---\ntitle: \"" + input + "\"\npublished: true\nmorea_coming_soon: false\nmorea_id: "+ input+"-module \nmorea_outcomes:\n  -"+input+"-outcome\nmorea_readings:\n  -"+input+"-reading\nmorea_experiences:\n  -"+input+"-experience\nmorea_assessments:\n  -"+input+"-assessment\nmorea_type: module\nmorea_icon_url:\nmorea_start_date:\nmorea_end_date:\nmorea_labels:\nmorea_sort_order:\n---\n\n## " + input + "\n\nThis is a sample content for the newly created .md file.";
                         outputStream.write(content.getBytes());
                     } catch (IOException ex) {
                         ex.printStackTrace();
