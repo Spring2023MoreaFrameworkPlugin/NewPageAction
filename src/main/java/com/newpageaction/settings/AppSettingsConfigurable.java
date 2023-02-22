cbf8510ec13543ae93aa04997f1b29931a931c67
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2023-02-22 00:12:01 -1000

    Created settings tab for defaults
    Does not fully function yet, only displays a settings tab and checkboxes for Outcomes, Readings, Experiences, and Assessments

---
+++ src/main/java/com/newpageaction/settings/AppSettingsConfigurable.java
@@ -0,0 +1,65 @@
+package com.newpageaction.settings;
+
+import com.intellij.openapi.options.Configurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Provides controller functionality for application settings.
+ */
+public class AppSettingsConfigurable implements Configurable {
+
+    private AppSettingsComponent mySettingsComponent;
+
+    // A default constructor with no arguments is required because this implementation
+    // is registered as an applicationConfigurable EP
+
+    @Nls(capitalization = Nls.Capitalization.Title)
+    @Override
+    public String getDisplayName() {
+        return "MOREA settings";
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        mySettingsComponent = new AppSettingsComponent();
+        return mySettingsComponent.getPanel();
+    }
+
+    @Override
+    public boolean isModified() {
+        AppSettingsState settings = AppSettingsState.getInstance();
+        boolean modified = mySettingsComponent.getOutcomeStates() != settings.outcomeStatus;
+        modified |= mySettingsComponent.getReadingStatus() != settings.readingStatus;
+        modified |= mySettingsComponent.getExperienceStatus() != settings.experienceStatus;
+        modified |= mySettingsComponent.getAssessmentStatus() != settings.assessmentStatus;
+        return modified;
+    }
+
+    @Override
+    public void apply() {
+        AppSettingsState settings = AppSettingsState.getInstance();
+        settings.outcomeStatus = mySettingsComponent.getOutcomeStates();
+        settings.readingStatus = mySettingsComponent.getOutcomeStates();
+        settings.experienceStatus = mySettingsComponent.getOutcomeStates();
+        settings.assessmentStatus = mySettingsComponent.getOutcomeStates();
+    }
+
+    @Override
+    public void reset() {
+        AppSettingsState settings = AppSettingsState.getInstance();
+        mySettingsComponent.setMyOutcomeStatus(settings.outcomeStatus);
+        mySettingsComponent.setMyReadingStatus(settings.outcomeStatus);
+        mySettingsComponent.setMyExperienceStatus(settings.outcomeStatus);
+        mySettingsComponent.setMyAssessmentStatus(settings.outcomeStatus);
+    }
+
+    @Override
+    public void disposeUIResources() {
+        mySettingsComponent = null;
+    }
+
+}
